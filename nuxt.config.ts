853a4eb8e26226646aa3a2a90778c217684fb3af
mustit-hyjang <94949361+mustit-hyjang@users.noreply.github.com>
2024-04-22 11:02:29 +0900

    nvm, scss, postcss 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'node:url';
+
 export default defineNuxtConfig({
     typescript: {
         typeCheck: true,
     },
     devtools: { enabled: true },
+    alias: {
+        '@': fileURLToPath(new URL('./', import.meta.url)),
+    },
+    ssr: true,
+    postcss: {
+        plugins: {
+            autoprefixer: {},
+        },
+    },
 });
