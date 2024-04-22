d1b871a44d9d91107629bb18911ed695799d1ed6
mustit-hyjang <94949361+mustit-hyjang@users.noreply.github.com>
2024-04-22 10:29:04 +0900

    eslint prettier 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  typescript: {
-    typeCheck: true
-  },
-  devtools: { enabled: true }
-})
+    typescript: {
+        typeCheck: true,
+    },
+    devtools: { enabled: true },
+});
